8fc699d0b87c78eef60d7407877d67d8866c4686
hroderik <100538342+hroderik@users.noreply.github.com>
2022-12-20 16:01:21 -0300

    Create ex001.cs

---
+++ ex001.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ex001
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int a, b, soma;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            soma = a + b;
+
+            Console.WriteLine($"SOMA = {soma}");
+        }
+    }
+}
