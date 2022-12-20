c488d1a508bb3efe89b36bd8fafd33521dd53d54
hroderik <100538342+hroderik@users.noreply.github.com>
2022-12-20 16:11:53 -0300

    Update ex002.cs

--- ex002.cs
+++ ex002.cs
@@ -6,10 +6,9 @@ namespace ex002
     internal class Program {
         static void Main(string[] args) {
 
-            double area, pi, raio;
+            double area, raio, pi = Math.PI;
 
             raio = double.Parse(Console.ReadLine());
-            pi = Math.PI;
             area = pi * Math.Pow(raio, 2);
 
             Console.WriteLine($"A={area.ToString("F4", CultureInfo.InvariantCulture)}");
