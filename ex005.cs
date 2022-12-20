18703857333c21ee837a133f5265da1fdb033e1c
hroderik <100538342+hroderik@users.noreply.github.com>
2022-12-20 17:59:01 -0300

    Update ex005.cs

--- ex005.cs
+++ ex005.cs
@@ -6,16 +6,15 @@ namespace ex005
     internal class Program {
         static void Main(string[] args) {
 
+            string[] var = Console.ReadLine().Split(' ');
+            int codPeca_1 = int.Parse(var[0]);
+            int numPeca_1 = int.Parse(var[1]);
+            double valorPeca_1 = double.Parse(var[2], CultureInfo.InvariantCulture);
 
-            string[] var_1 = Console.ReadLine().Split(' ');
-            int codPeca_1 = int.Parse(var_1[0]);
-            int numPeca_1 = int.Parse(var_1[1]);
-            double valorPeca_1 = double.Parse(var_1[2]);
-
-            string[] var_2 = Console.ReadLine().Split(' ');
-            int codPeca_2 = int.Parse(var_2[0]);
-            int numPeca_2 = int.Parse(var_2[1]);
-            double valorPeca_2 = double.Parse(var_2[2]);
+            var = Console.ReadLine().Split(' ');
+            int codPeca_2 = int.Parse(var[0]);
+            int numPeca_2 = int.Parse(var[1]);
+            double valorPeca_2 = double.Parse(var[2], CultureInfo.InvariantCulture);
 
             double pagar = (numPeca_1 * valorPeca_1) + (numPeca_2 * valorPeca_2);
 
