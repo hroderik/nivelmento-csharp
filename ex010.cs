3b031474959db439b256036e879a084729812642
hroderik <100538342+hroderik@users.noreply.github.com>
2022-12-21 00:35:20 -0300

    Create ex010.cs

---
+++ ex010.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace ex010
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+            int horas;
+
+            if (a > b) {
+                horas = 24 - a + b;
+            }
+            else if (a == b) {
+                horas = 24 - a + b;
+            }
+            else {
+                horas = b - a;
+            }
+            
+            Console.WriteLine($"O JOGO DUROU {horas} HORA(S)");
+        }
+    }
+}
