d75d6f1559dd92f2d8dcab1885382db323a9160d
hroderik <100538342+hroderik@users.noreply.github.com>
2022-12-21 22:59:08 -0300

    Create ex012.cs

---
+++ ex012.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Globalization;
+
+namespace ex012 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double entrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (entrada < 0 || entrada > 100) {
+                Console.WriteLine("Fora de intervalo");
+            }
+            else if (entrada <= 25) {
+                Console.WriteLine("Intervalo (0,25]");
+            }
+            else if (entrada <= 50) {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            else if (entrada <= 75) {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+        }
+    }
+}
